f81407a0a3341285558561b21dbee25ccd0edf8f
Ishotihadus <webmaster@ishotihadus.com>
2026-07-09 15:06:09 +0900

    Update

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,18 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
+    python = { "ruff_organize_imports", "ruff_format" },
+    ruby = { "rubocop" },
     -- css = { "prettier" },
     -- html = { "prettier" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 10000,
+    lsp_fallback = true,
+    quiet = false,
+  },
 }
 
 return options
--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,25 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+vim.lsp.config("basedpyright", {
+  cmd = {
+    "bunx",
+    "-p",
+    "basedpyright@latest",
+    "basedpyright-langserver",
+    "--stdio",
+  },
+  settings = {
+    basedpyright = {
+      analysis = {
+        typeCheckingMode = "basic",
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
+      },
+    },
+  },
+})
+
+local servers = { "html", "cssls", "basedpyright", "solargraph" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
